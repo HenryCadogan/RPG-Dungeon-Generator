d03b2c72b233c88284de45125631274ec9c0fe94
Henry Cadogan <Henry.Cadogan@gmail.com>
2019-02-24 18:32:04 +0000

    Started method for oneOrMore

---
+++ src/main/kotlin/grammar/operators/OneOrMore.kt
@@ -0,0 +1,12 @@
+package grammar.operators
+
+import grammar.GrammarItem
+
+fun oneOrMore(item: GrammarItem, limit:Int):List<GrammarItem>{
+    val output = MutableList<GrammarItem>()
+    for (x in 0..limit){
+                
+    }
+}
+
+
