eff74c9adef27ad34843962b876a7e3ede94e727
HenryCadogan <hgc509@york.ac.uk>
2019-04-24 14:57:46 +0100

    Fixed tests

--- src/test/kotlin/grammar/operators/OneOfKtTest.kt
+++ src/test/kotlin/grammar/operators/OneOfKtTest.kt
@@ -4,14 +4,19 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.reset
 import com.nhaarman.mockitokotlin2.whenever
+import grammar.Constraints
+import grammar.Factories
 import grammar.grammarItems.GrammarItem
+import grammar.grammarItems.factories.DungeonRoomFactory
+import grammar.grammarItems.rooms.DungeonRoom
 import org.testng.Assert
 import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
+import theme.Theme
 import kotlin.random.Random
 
 
-class TestItem(terminal:Boolean = true): GrammarItem(terminal)
+class TestItem(terminal:Boolean = true): DungeonRoom(terminal)
 
 class OneOfKtTest {
     private val rnd : Random = mock()
@@ -28,11 +33,13 @@ class OneOfKtTest {
         val itemB = TestItem()
         whenever(rnd.nextInt(any(),any())).thenReturn(1)
         val result1 = ops.oneOf.oneOf(listOf(itemA,itemB))
-        Assert.assertEquals(result1, itemA)
+        Assert.assertEquals(result1.size,1)
+        Assert.assertEquals(result1.first(), itemA)
 
         whenever(rnd.nextInt(any(),any())).thenReturn(2)
         val result = ops.oneOf.oneOf(listOf(itemA,itemB))
-        Assert.assertEquals(result, itemB)
+        Assert.assertEquals(result.size,1)
+        Assert.assertEquals(result.first(), itemB)
     }
 
     @Test
@@ -43,13 +50,16 @@ class OneOfKtTest {
         val items = mapOf<GrammarItem,Int>(itemA to 2,itemB to 1,itemC to 3)
         whenever(rnd.nextInt(any(),any())).thenReturn(6)
         val result1 = ops.oneOf.oneOf(items)
-        Assert.assertEquals(result1,itemC)
+        Assert.assertEquals(result1.size,1)
+        Assert.assertEquals(result1.first(),itemC)
         whenever(rnd.nextInt(any(),any())).thenReturn(3)
         val result2 = ops.oneOf.oneOf(items)
-        Assert.assertEquals(result2,itemA)
+        Assert.assertEquals(result2.size,1)
+        Assert.assertEquals(result2.first(),itemA)
         whenever(rnd.nextInt(any(),any())).thenReturn(1)
         val result3 = ops.oneOf.oneOf(items)
-        Assert.assertEquals(result3,itemB)
+        Assert.assertEquals(result3.size,1)
+        Assert.assertEquals(result3.first(),itemB)
     }
 
 
@@ -58,6 +68,7 @@ class OneOfKtTest {
         val itemA = TestItem(false)
         whenever(rnd.nextInt(any(),any())).thenReturn(1)
         val result = ops.oneOf.oneOf(listOf(itemA))
-        Assert.assertEquals(result,itemA)
+        Assert.assertEquals(result.size,1)
+        Assert.assertEquals(result.first(),itemA)
     }
 }
--- src/test/kotlin/grammar/operators/OneOrMoreKtTest.kt
+++ src/test/kotlin/grammar/operators/OneOrMoreKtTest.kt
@@ -3,9 +3,14 @@ package grammar.operators
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.reset
 import com.nhaarman.mockitokotlin2.whenever
+import grammar.Factories
+import grammar.grammarItems.factories.Descriptions
+import grammar.grammarItems.factories.DungeonRoomFactory
+import grammar.grammarItems.rooms.DungeonRoom
 import org.testng.Assert
 import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
+import theme.Theme
 import kotlin.random.Random
 
 
@@ -13,27 +18,30 @@ class OneOrMoreKtTest{
 
     private val rnd: Random = mock()
     private val operators = GrammarOperators(rnd)
+    private val roomFactory= DungeonRoomFactory(Theme.AQUATIC)
 
     @BeforeTest
     fun setup(){
         reset(rnd)
+        Factories.roomFactory = roomFactory
     }
+
     @Test
     fun `it should return a list containing Items of the class specified`(){
-        val item = TestItem(false)
+        val item = DungeonRoom(false)
         whenever(rnd.nextFloat()).thenReturn(0f)
         val output = operators.oneOrMore.oneOrMore(item,1,1f)
-        Assert.assertTrue(output.first() is TestItem)
+        Assert.assertTrue(output.first() is DungeonRoom)
     }
 
     @Test
     fun `it should return a list containing multiple Items of the class specified`(){
-        val item = TestItem(false)
+        val item = DungeonRoom(false)
         val output =operators.oneOrMore.oneOrMore(item,2,1f)
         whenever(rnd.nextFloat()).thenReturn(1f)
         Assert.assertEquals(output.size,2)
-        Assert.assertTrue(output.first() is TestItem)
-        Assert.assertTrue(output[1] is TestItem)
+        Assert.assertTrue(output.first() is DungeonRoom)
+        Assert.assertTrue(output[1] is DungeonRoom)
         Assert.assertNotEquals(output.first(),output[1])
     }
 
